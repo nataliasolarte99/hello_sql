321b1f169db6d111f571002966da092bbbc31422
Natalia Solarte Rivera <nataliasolarterivera@gmail.com>
2025-06-02 16:04:07 +0200

    Create Select.sql

---
+++ Select.sql
@@ -0,0 +1,9 @@
+
+-- Gets all data from the "users" table
+SELECT * FROM users;
+
+-- Gets all names from the "users" table
+SELECT name FROM users;
+
+-- Gets all IDs and names from the "users" table
+SELECT user_id, name FROM users;
